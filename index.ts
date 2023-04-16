a10d66a7c2681d13f16d2f5e796b140214061bcb
dj-viking <anders.swedishviking@gmail.com>
2023-04-16 13:34:47 -0400

    accidentally deleted index.html

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 class Main {
+    public rootEl: HTMLDivElement;
+    public constructor() {
+        this.rootEl = document.querySelector("#root") as HTMLDivElement;
+    }
     public run(): void {
         console.log("running main");
     }
