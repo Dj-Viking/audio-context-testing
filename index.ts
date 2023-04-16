26080630668fc1087fa7fea85f74b136051fb39e
dj-viking <anders.swedishviking@gmail.com>
2023-04-16 13:36:03 -0400

    making audio context

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 class Main {
     public rootEl: HTMLDivElement;
+    public audioCtx: AudioContext;
     public constructor() {
         this.rootEl = document.querySelector("#root") as HTMLDivElement;
+        this.audioCtx = new AudioContext();
+        console.log("my ctx", this.audioCtx);
     }
     public run(): void {
         console.log("running main");
